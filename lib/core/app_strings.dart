a20cba1ebcd80ad20e531b1e13278528314192d7
MD ZAHED IQBAL <tajiniiuc222@gmail.com>
2026-03-02 15:29:21 +0600

    app colors and strings file added

---
+++ lib/core/app_strings.dart
@@ -0,0 +1,37 @@
+class AppStrings {
+  static const String appName = 'Recipe App';
+
+  // API Constants
+  static const String apiKey = '93ed1c51fb2745339440843df604c23a';
+
+  // UI Strings
+  static const String categories = 'Categories';
+  static const String searchRecipes = 'Search Recipes';
+  static const String favorites = 'Favorites';
+  static const String ingredients = 'Ingredients';
+  static const String instructions = 'Instructions';
+  static const String noRecipesFound = 'No recipes found.';
+  static const String noFavoritesYet = 'No favorite recipes yet.';
+  static const String noInstructions =
+      'Instructions not available for this recipe.';
+  static const String searchHint = 'Search for pasta, chicken...';
+
+  // Newly Added UI Strings
+  static const String welcomeBack = 'Welcome Back ';
+  static const String letsCook = 'Let\'s Cook!';
+  static const String recipeDetails = 'Recipe Details';
+  static const String failedToLoadRecipe = 'Failed to load recipe details.';
+  static const String typeToSearch = 'Type to search for recipes.';
+  static const String defaultRating = '5.0';
+  static const String mins = ' mins';
+  static const String servings = ' servings';
+
+  // Constants
+  static const List<String> recipeCategories = [
+    'Italian',
+    'Mexican',
+    'Asian',
+    'American',
+    'Indian',
+  ];
+}
---
+++ lib/core/urls.dart
@@ -0,0 +1,5 @@
+class Urls {
+  static const String baseUrl = 'https://api.spoonacular.com/recipes';
+  static const String profileImageUrl =
+      'https://images.stockcake.com/public/e/7/1/e71ca2d8-cda0-461f-9d70-1e3c0dcb2d3f_large/handsome-man-portrait-stockcake.jpg';
+}
